13565fa5e1f6d44523c2af303b644d428f3e1abd
witwave <witwave@outlook.com>
2016-02-24 16:51:32 +0800

    ...

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,7 +6,7 @@
 |--------------------------------------------------------------------------
 */
   Route::get('/', function() {
-       return view('welcome');
+      return redirect('admin/dashboard');
     });
 
 
